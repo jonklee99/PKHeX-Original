7970016712c9879584357ee71822c90c703ac34c
Jon <jonkurtislee@yahoo.com>
2026-01-18 15:40:22 -0600

    Change default OT to Jon

--- PKHeX.Core/PKM/Util/TrainerName.cs
+++ PKHeX.Core/PKM/Util/TrainerName.cs
@@ -8,6 +8,6 @@ public static class TrainerName
     public const string GameFreakJPN = "ゲーフリ";
     public const string GameFreakINT = "GF";
 
-    public const string ProgramJPN = "ＰＫＨｅＸ";
-    public const string ProgramINT = "PKHeX";
+    public const string ProgramJPN = "ジョン";
+    public const string ProgramINT = "Jon";
 }
